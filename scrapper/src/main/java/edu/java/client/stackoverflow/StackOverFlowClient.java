76eb4ccac408280734741c2761ec1b0cf6246b59
AlexVolkov <AlexVolkovProgrammer@yandex.com>
2024-03-03 18:47:41 +0300

    addSOFClient

---
+++ scrapper/src/main/java/edu/java/client/stackoverflow/StackOverFlowClient.java
@@ -0,0 +1,11 @@
+package edu.java.client.stackoverflow;
+
+import edu.java.client.stackoverflow.dto.ListAnswerResponse;
+import edu.java.client.stackoverflow.dto.ListCommentsResponse;
+import edu.java.client.stackoverflow.dto.ListRelatedQuestionsResponse;
+
+public interface StackOverFlowClient {
+    ListAnswerResponse fetchAnswerEvents(String questionId);
+    ListCommentsResponse fetchCommentEvents(String questionId);
+    ListRelatedQuestionsResponse fetchRelatedQuestionEvent(String questionId);
+}
---
+++ scrapper/src/main/java/edu/java/client/stackoverflow/dto/ListAnswerResponse.java
@@ -0,0 +1,20 @@
+package edu.java.client.stackoverflow.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.OffsetDateTime;
+import java.util.List;
+
+public class ListAnswerResponse {
+    @JsonProperty("items")
+    private List<AnswerEventResponse> answers;
+
+    public record AnswerEventResponse(
+        @JsonProperty("owner") UserInformationResponse owner,
+        @JsonProperty("answer_id") long answerId,
+        @JsonProperty("is_accepted") boolean isAccepted,
+        @JsonProperty("score") int score,
+        @JsonProperty("creation_date") OffsetDateTime creationDate,
+        @JsonProperty("body") String body
+        ){
+    }
+}
